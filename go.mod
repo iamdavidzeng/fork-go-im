08f92495d2b43d499c263c92012e6cdd344ed567
zzy <15779712170@163.com>
2021-09-02 09:08:51 +0800

    Add message controller

--- go.mod
+++ go.mod
@@ -4,8 +4,18 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang-module/carbon v1.5.3
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/spf13/cast v1.4.1
 	github.com/spf13/viper v1.8.1
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.14
 )
---
+++ im/http/controller/im/message.go
@@ -0,0 +1,102 @@
+package im
+
+import (
+	"fmt"
+	userModel "fork_go_im/im/http/models/user"
+	"fork_go_im/pkg/helper"
+	"fork_go_im/pkg/model"
+	"fork_go_im/pkg/response"
+	"net/http"
+	"sort"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-module/carbon"
+	"github.com/spf13/cast"
+)
+
+type MessageController struct{}
+
+type ImMessage struct {
+	ID          uint64          `json:"id"`
+	Msg         string          `json:"msg"`
+	CreatedAt   string          `json:"created_at"`
+	FromId      uint64          `json:"user_id"`
+	ToId        uint64          `json:"send_id"`
+	Channel     string          `json:"channel"`
+	Status      int             `json:"is_read"`
+	MsgType     int             `json:"msg_type"`
+	ChannelType int             `json:"channel_type"`
+	Users       userModel.Users `json:"users" gorm:"foreignKey:FromId;references:ID"`
+}
+
+func (*MessageController) InfomationHistory(c *gin.Context) {
+	toId := c.Query("to_id")
+	channdelType := c.DefaultQuery("channdel_type", "1")
+	user := userModel.AuthUser
+	fromId := cast.ToString(user.ID)
+	if len(toId) < 0 {
+		response.FailResponse(http.StatusBadRequest, "User id cannot be null.").ToJson(c)
+	}
+
+	var MsgList []ImMessage
+	channelA, channelB := helper.ProduceChannelName(fromId, toId)
+	fmt.Println(channelA, channelB)
+	list := model.DB.Model(ImMessage{}).
+		Where("(channel = ? or channel = ?) amd channel_type = ? order by created_at desc", channelA, channelB, channdelType).
+		Limit(40).
+		Select("id, msg, created_at, from_id, to_id, channel, msg_type").
+		Find(&MsgList)
+	if list.Error != nil {
+		return
+	}
+
+	fromIds, _ := cast.ToUint64E(user.ID)
+	for key, value := range MsgList {
+		MsgList[key].CreatedAt = carbon.Parse(value.CreatedAt).SetLocale("zh-CN").DiffForHumans()
+		if value.FromId == fromIds {
+			MsgList[key].Status = 0
+		} else {
+			MsgList[key].Status = 1
+		}
+	}
+	SortByAge(MsgList)
+	response.SuccessResponse(MsgList, 200).ToJson(c)
+}
+
+func SortByAge(list []ImMessage) {
+	sort.Slice(list, func(i, j int) bool {
+		return list[i].ID < list[j].ID
+	})
+}
+
+func (*MessageController) GetGroupMessageList(c *gin.Context) {
+	toId := c.Query("to_id")
+	channelType := c.DefaultQuery("channel_type", "1")
+	user := userModel.AuthUser
+
+	if len(toId) < 0 {
+		response.FailResponse(500, "User id cannot be null.").ToJson(c)
+	}
+
+	var MsgList []ImMessage
+	channelA := helper.ProduceChannelGroupName(toId)
+	list := model.DB.Preload("Users").
+		Where("channel = ? and channel_type = ? order by created_at desc", channelA, channelType).
+		Limit(40).
+		Select("id, msg, created_at, from_id, to_id, channel, msg_type").
+		Find(&MsgList)
+	if list.Error != nil {
+		return
+	}
+	fromIds, _ := cast.ToUint64E(user.ID)
+	for key, value := range MsgList {
+		MsgList[key].CreatedAt = carbon.Parse(value.CreatedAt).SetLocale("zh-CN").DiffForHumans()
+		if value.FromId == fromIds {
+			MsgList[key].Status = 0
+		} else {
+			MsgList[key].Status = 1
+		}
+	}
+	SortByAge(MsgList)
+	response.SuccessResponse(MsgList, 200).ToJson(c)
+}
